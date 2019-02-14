574d240770baccd57a68d10a86773a720fe2099f
Sumedh Shende <sshende@splunk.com>
2019-02-13 17:42:10 -0800

    template cleanup and level print

--- src/bst.cpp
+++ src/bst.cpp
@@ -1,148 +1,123 @@
 // bst.cpp : Defines the entry point for the console application.
 //
 
+#include "stdafx.h"
 #include<cstdio>
 #include<iostream>
-#include<iomanip>
-#include <vector>
+#include<vector>
+#include <list>
 #include <cmath>
+
 using namespace std;
 
-template<class T>
 struct bnode {
     bnode* left;
     bnode* right;
-    T val;
+    int val;
     bnode() :left(nullptr), right(nullptr) {}
 };
 
-template<class T>
+
 class bstree {
+    bnode* _r;
 public:
-    
-    bnode<T>* buildbst() {
-        bnode<T>*r = new bnode<T>;
+    bstree():_r(nullptr) {
+
+    }
+    bnode* buildbst() {
+        bnode*r = new bnode;
         r->val = 5;
-        r->left = new bnode<T>;
+        r->left = new bnode;
         r->left->val = 2;
-        r->right = new bnode<T>;
+        r->right = new bnode;
         r->right->val = 8;
 
-        r->left->left = new bnode<T>;
+        r->left->left = new bnode;
         r->left->left->val = 1;
-        r->left->right = new bnode<T>;
+        r->left->right = new bnode;
         r->left->right->val = 3;
 
-        r->right->left = new bnode<T>;
+        r->right->left = new bnode;
         r->right->left->val = 7;
-        r->right->right = new bnode<T>;
+        r->right->right = new bnode;
         r->right->right->val = 9;
+        _r = r;
         return r;
     }
-    void preorder(bnode<T>* r) {
-        if (r == nullptr) { 
-            return;
-        }
-        cout << r->val << " ";
-        preorder(r->left);
-        preorder(r->right);
+    void preorder(vector<int>& vA,bnode* r) {
+        if (r == nullptr) return;
+        vA.push_back(r->val);
+        preorder(vA, r->left);
+        preorder(vA, r->right);
     }
-    void postorder(bnode<T>* p, int indent=0)
-    {
-        if(p != NULL) {
-            if(p->left) postorder(p->left, indent+4);
-            if(p->right) postorder(p->right, indent+4);
-            if (indent) {
-                std::cout << setw(indent) << ' ';
-            }
-            cout<< p->val;
-        }
-    }
-    void clean(bnode<T>** r) {
-        bnode<T>* t = *r;
+    void clean(bnode** r) {
+        bnode* t = *r;
         if (t->left) clean(&t->left);
         if (t->right) clean(&t->right);
         delete t;
-        *r = nullptr;
+       *r = nullptr;
     }
-    void pprint(bnode<T>* r) {
-        vector<T> v;
-        vector<bnode<T>*> q;
-        if (r == nullptr) return;
-        q.push_back(r);
-        v.push_back(n->val);
-        while (!q.empty()) {
-            bnode<T>* n = q[0];
-            q.erase(q.begin());
-            if (n->left) {
-                v.push_back(n->left->val);
-                q.push_back(n->left);
-            }else{
-                  v.push_back(0);
-            }
-            if (n->right) {
-                v.push_back(n->right->val);
-                q.push_back(n->right);
-            }
-            else {
-                v.push_back(0);
-            }
+    bnode* insert(int val, bnode* r) {
+          if (r == nullptr) {
+            r = new bnode;
+            r->val = val;;
         }
-        int indent = 50;
-        setw(indent);
-       /* cout << v[0] << endl;*/
-        for (int i=0, d = 0; i < v.size(); i++, d++) {
-            int count = pow(2,d);
-            
-            for (int j = 0; (j < count && i < v.size()); j++) {
-                cout << v[i++];
-            }
-           // indent /= 2;
-            cout << endl;
-        }
-    }
-    //            5
-    ////      2       8
-    ///   1      3 7     9
-    //
-
-    bnode<T>* insert(bnode<T>* r, T val){
-        if(r == nullptr) {
-            r = new bnode<T>;
-            r->val = val;
-        };
-        
-        if(val < r->val) {
-            r->left =  insert(r->left, val);
-        } else if(val > r->val) {
-            r->right = insert(r->right, val);
-        }
+        if (val < r->val)
+          r->left = insert(val, r->left);
+        if (val > r->val)
+          r->right = insert(val, r->right);
         return r;
+    }
+    void pprint() {
+        vector<int> v;
+        list<bnode*> l;
+        bnode* t = _r;
+        if (t == nullptr) return;
+        l.push_back(t);
+
+        while (!l.empty()) {
+            bnode* t = l.front();
+            l.pop_front();
+            if (t->left) {
+                l.push_back(t->left);
+            }
+            if (t->right) {
+                l.push_back(t->right);
+            }
+            v.push_back(t->val);
+        }
+        int level = 0;
+        for (int i = 0; i < v.size(); i++) {
+            cout << v[i] << " ";
+        }
+    }
+};
+class test_bst {
+public:
+    
+    bool test_clean() {
+        bstree bst;
+        bnode* r = bst.buildbst();
+        vector<int> v1;
+        vector<int> v2;
+       
+        bst.pprint(); 
+        bst.preorder(v1, r);
+        bst.clean(&r);
+        bst.preorder(v2, r);
+        bst.pprint();
+        if (v2.size() == 0) {
+            return true;
+        }
+        return false;
     }
 };
 
 int main()
 {
-    bstree<int> bst;
-    bnode<int>* r = bst.buildbst();
-  
-    bst.pprint(r);
-    bst.insert(r, 4);
-   // bst.preorder(r);
-  //  cout << endl;
-    bst.pprint(r);
-    bst.insert(r, 6);
-  //  bst.preorder(r);
-   // cout << endl;
-    bst.pprint(r);
-   bst.insert(r, 11);
-  // bst.preorder(r);
-  // cout << endl;
-   bst.pprint(r);
-   bst.insert(r, 0);
-   bst.pprint(r);
-  // bst.preorder(r);
-  // cout << endl;
-return 0;
+    test_bst tbst;
+    cout << "Test bstree::clean() : " << (tbst.test_clean() ? "PASS": "FAIL");
+   
+    return 0;
 }
 
